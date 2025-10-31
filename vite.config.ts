e96dde9172d84703a27a39ab23078e36e6fc3c10
Dean Lofts <dean@deanlofts.xyz>
2025-10-31 21:46:36 +1100

    pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-pizza-demo/',
   plugins: [mdx(), tailwindcss(), react()],
 })
